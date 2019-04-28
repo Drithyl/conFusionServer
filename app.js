33328577e6382dcbac78e9c1592c3694b46056a5
Drithyl <hugo.bujan@gmail.com>
2019-04-28 17:25:55 -0400

    Basic Authentication

--- app.js
+++ app.js
@@ -33,6 +33,58 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+//authorization middleware
+function auth(req, res, next)
+{
+  console.log(req.headers);
+
+  //grabs authorization header sent by client
+  let authHeader = req.headers.authorization;
+
+  //username and password were not included
+  if (authHeader == null)
+  {
+    let err = new Error(`You are not authenticated!`);
+
+    res.setHeader(`WWW-Authenticate`, `Basic`);
+    err.status = 401; //Unauthorized access code
+
+    //skip all other middlewares below and send error since client is not authenticated
+    return next(err);
+  }
+
+  //header contais "Basic username:password", so we split by a space
+  //and keep only the username:password bit, which is encoded in base64,
+  //so we convert it into a buffer with that encoding and then back to string
+  //so that it is readable. Next we split by ":" to get an array containing
+  //username and password
+  let auth = new Buffer(authHeader.split(` `)[1], `base64`).toString().split(`:`);
+  let username = auth[0];
+  let password = auth[1];
+
+  if (username === "admin" && password === "password")
+  {
+    //allow user to pass down to the next middleware to service the request
+    next();
+  }
+
+  else
+  {
+    let err = new Error(`You are not authenticated!`);
+
+    res.setHeader(`WWW-Authenticate`, `Basic`);
+    err.status = 401; //Unauthorized access code
+
+    //skip all other middlewares below and send error since client is not authenticated
+    return next(err);
+  }
+}
+
+app.use(auth);
+
+//enables us to serve static data from our public resources
+//authentication should come before this
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
