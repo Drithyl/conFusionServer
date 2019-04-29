99833942ef2d7cfaac5c048d485b70793799e6d3
Drithyl <hugo.bujan@gmail.com>
2019-04-28 20:50:19 -0400

    Cookie-based authentication

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
+
+/**************************************
+*   THIS IS A SCAFFOLDED EXPRESS APP  *
+**************************************/
+
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
@@ -32,52 +37,90 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+
+//Pass a string to use as a secret key for signed cookies
+app.use(cookieParser(`12345-67890-09876-54321`));
 
 //authorization middleware
 function auth(req, res, next)
 {
-  console.log(req.headers);
+  console.log(`List of headers included in the request: \n\n`, req.headers);
+  console.log(`Signed Cookies included: \n\n`, req.signedCookies);
 
-  //grabs authorization header sent by client
-  let authHeader = req.headers.authorization;
-
-  //username and password were not included
-  if (authHeader == null)
+  //If the incoming request has no user field in the signed cookies
+  //(as declared further below when setting the cookie),
+  //it will mean that the user has not been authorized yet, so we will
+  //expect him to sign in
+  if (req.signedCookies.user == null)
   {
-    let err = new Error(`You are not authenticated!`);
+    //grabs authorization header sent by client
+    let authHeader = req.headers.authorization;
 
-    res.setHeader(`WWW-Authenticate`, `Basic`);
-    err.status = 401; //Unauthorized access code
+    //username and password were not included
+    if (authHeader == null)
+    {
+      let err = new Error(`You are not authenticated!`);
 
-    //skip all other middlewares below and send error since client is not authenticated
-    return next(err);
+      //Including this header in the response will prompt the user to authorize himself
+      res.setHeader(`WWW-Authenticate`, `Basic`);
+      err.status = 401; //Unauthorized access code
+
+      //skip all other middlewares below and send error since client is not authenticated
+      return next(err);
+    }
+
+    //header contais "Basic username:password", so we split by a space
+    //and keep only the username:password bit, which is encoded in base64,
+    //so we convert it into a buffer with that encoding and then back to string
+    //so that it is readable. Next we split by ":" to get an array containing
+    //username and password
+    let auth = new Buffer.from(authHeader.split(` `)[1], `base64`).toString().split(`:`);
+    let username = auth[0];
+    let password = auth[1];
+
+    if (username === "admin" && password === "password")
+    {
+      //This cookie name we set here will be accessible in the req object though
+      //req.signedCookies.user. This way once the user is authenticated once,
+      //we can read the cookie and not require authentication each time
+      //here it sets the key user to the value admin, req.signedCookies.user === "admin"
+      res.cookie(`user`, `admin`, { signed: true })
+
+      //allow user to pass down to the next middleware to service the request
+      next();
+    }
+
+    else
+    {
+      let err = new Error(`You are not authenticated!`);
+
+      res.setHeader(`WWW-Authenticate`, `Basic`);
+      err.status = 401; //Unauthorized access code
+
+      //skip all other middlewares below and send error since client is not authenticated
+      return next(err);
+    }
   }
 
-  //header contais "Basic username:password", so we split by a space
-  //and keep only the username:password bit, which is encoded in base64,
-  //so we convert it into a buffer with that encoding and then back to string
-  //so that it is readable. Next we split by ":" to get an array containing
-  //username and password
-  let auth = new Buffer(authHeader.split(` `)[1], `base64`).toString().split(`:`);
-  let username = auth[0];
-  let password = auth[1];
-
-  if (username === "admin" && password === "password")
-  {
-    //allow user to pass down to the next middleware to service the request
-    next();
-  }
-
+  //signed cookie exists in the request
   else
   {
-    let err = new Error(`You are not authenticated!`);
+    if (req.signedCookies.user === `admin`)
+    {
+      //allow access
+      next();
+    }
 
-    res.setHeader(`WWW-Authenticate`, `Basic`);
-    err.status = 401; //Unauthorized access code
+    else
+    {
+      //not admin
+      let err = new Error(`You are not authenticated!`);
 
-    //skip all other middlewares below and send error since client is not authenticated
-    return next(err);
+      err.status = 401; //Unauthorized access code
+
+      //skip all other middlewares below and send error since client is not authenticated
+      return next(err);
+    }
   }
 }
 
